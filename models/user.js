bbbef2a8d6f31e4ed441c56906534dfb69fcce10
arunpratapsingh17 <218apsj@gmail.com>
2019-12-23 05:17:04 +0000

    Second Update

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ var mongoose=require("mongoose");
 var passportlocalMongoose=require("passport-local-mongoose");
 var userSchema=new mongoose.Schema({
 	username:String,
-	password:String
+	password:String,
+	isAdmin:{type:Boolean,default:false}
 });
 userSchema.plugin(passportlocalMongoose);
 module.exports=mongoose.model("User",userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ router.get("/register",function(req,res){
 });
 router.post("/register",function(req,res){
 	var newUser=new User({username:req.body.username});
+	
 	User.register(newUser,req.body.password,function(err,user){
 		if(err)
 			{
